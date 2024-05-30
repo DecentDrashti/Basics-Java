cb8d84710dd3238f24c0c817a1f09c379916194f
DecentDrashti <drashtiruparelia.678@gmail.com>
2024-05-30 22:03:44 +0530

    2d array

--- 2d array/Array2d_Demo.java
+++ 2d array/Array2d_Demo.java
@@ -1,10 +1,18 @@
 public class Array2d_Demo{
    public static void main(String[] args) {
-     int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
-     for (int i = 0; i < myNumbers.length; ++i) {
-        for(int j = 0; j < myNumbers[i].length; ++j) {
-           System.out.println(myNumbers[i][j]);
+     int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7, 8} };
+     for (int i = 0; i < myNumbers.length; i++) {
+      System.out.print("|");
+        for(int j = 0; j < myNumbers[i].length; j++) {
+         if(j>=1){
+         System.out.print("\t");
+         }
+          System.out.print(myNumbers[i][j]);
+         //  if(j<4){
+          System.out.print("\t");
+         //  }
         }
+         System.out.println("\b\b\b\b\b\b|");
      }
    }
 }
