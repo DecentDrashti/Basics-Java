6d0cbd2150a86f585c09ccaac859450663b2e97f
DecentDrashti <drashtiruparelia.678@gmail.com>
2024-06-03 12:16:08 +0530

    linkedlist

--- Linkedlist class/L2(remaining).java
+++
@@ -1,3 +0,0 @@
-//Linked list method 
-LinkedList.get(index no.)
-LinkedList.size()=size of list
---
+++ Linkedlist class/L2.java
@@ -0,0 +1,10 @@
+//Linked list method 
+ LinkedList<String> cars = new LinkedList<String>();
+cars.get(index no.)
+cars.size()=size of list
+addFirst()	Adds an item to the beginning of the list.
+addLast()	Add an item to the end of the list.
+removeFirst()	Remove an item from the beginning of the list.
+removeLast()	Remove an item from the end of the list
+getFirst()	Get the item at the beginning of the list.
+getLast()	Get the item at the end of the list.
