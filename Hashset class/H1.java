2b539af5989ad5873364783b2bb852d81506e332
DecentDrashti <drashtiruparelia.678@gmail.com>
2024-06-08 19:56:20 +0530

    Hashset class

---
+++ Hashset class/H1.java
@@ -0,0 +1,19 @@
+//A HashSet is a collection of items where every item is unique, and it is found in the java.util package.
+// Import the HashSet class
+import java.util.HashSet;
+
+public class Main {
+  public static void main(String[] args) {
+    HashSet<String> cars = new HashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
+//methods are contains() remove() and clear().
+//contains is boolean method
+//remove removes an single element.
+//clear removes all elements.
