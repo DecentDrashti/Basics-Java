517fd76e25f0c58fa7fce2c7886b9fec44ed5b3b
Drashti Ruparelia <155230292+DecentDrashti@users.noreply.github.com>
2026-06-18 19:38:19 +0530

    Add Inheritance demo with Animal and Dog classes

---
+++ Inheritance/Inheritance_Demo.java
@@ -0,0 +1,27 @@
+// Parent Class
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+// Child Class
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class Inheritance_Demo {
+
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog();
+
+        // Method from parent class
+        myDog.eat();
+
+        // Method from child class
+        myDog.bark();
+    }
+}
