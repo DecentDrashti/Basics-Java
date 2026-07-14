4fa077ed259206d4c21722f70e44b88f96dce03e
Drashti Ruparelia <155230292+DecentDrashti@users.noreply.github.com>
2026-07-14 18:32:38 +0530

    Add string concatenation examples in 03_Strings_Methods
    Added examples of string concatenation using both '+' operator and concat() method.

--- String/03_Strings_Methods.java
+++ String/03_Strings_Methods.java
@@ -19,6 +19,27 @@ public class String_Methods {
 
         System.out.println(text.replace("Java", "World"));
 
+       /* ==================================================
+            String Concatenation IN JAVA
+        ==================================================*/
+        String firstName = "Drashti";
+        String lastName = "Ruparelia";
+
+        System.out.println(firstName + " " + lastName);
+
+        System.out.println(firstName.concat(" " + lastName));
+
+
     }
 
 }
+//output:
+// 10
+// HELLO JAVA
+// hello java
+// e
+// 6
+// true
+// Hello World
+//Drashti Ruparelia
+//Drashti Ruparelia
