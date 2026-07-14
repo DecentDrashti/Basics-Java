8a28058d1e5549743a0503dabe1c8fef59b70942
Drashti Ruparelia <155230292+DecentDrashti@users.noreply.github.com>
2026-07-14 18:16:39 +0530

    Create What_is _String.java

---
+++ String/What_is _String.java
@@ -0,0 +1,52 @@
+/*
+==================================================
+                STRING IN JAVA
+==================================================
+
+A String is a sequence of characters used to store
+and manipulate text.
+
+Examples:
+"Hello"
+"Java"
+"Welcome"
+
+Unlike primitive data types, String is an object.
+
+Strings in Java are immutable, which means once a
+String object is created, its value cannot be changed.
+
+--------------------------------------------------
+
+Ways to Create a String
+
+1. Using String Literal
+
+String name = "Drashti";
+
+2. Using new Keyword
+
+String name = new String("Drashti");
+
+--------------------------------------------------
+
+Why do we use Strings?
+
+✔ Store names
+✔ Store addresses
+✔ Store passwords
+✔ Store emails
+✔ Display messages
+
+==================================================
+*/
+
+public class What_Is_String {
+
+    public static void main(String[] args) {
+
+        String language = "Java";
+
+        System.out.println("Programming Language : " + language);
+    }
+}
